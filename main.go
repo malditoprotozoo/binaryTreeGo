8ca4b36f9f3e62e29691368baedcdc10f8b115c8
malditoprotozoo <virodriguez.fe@gmail.com>
2018-05-09 16:49:17 -0300

    adding remove method

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Node : create a type node
 type Node struct {
@@ -15,26 +17,24 @@ type Tree struct {
 	Root *Node
 }
 
-// InsertRoot : If there's no root on the tree, this function adds one
-func (tree *Tree) InsertRoot(key int, data string) {
-	tree.Root = &Node{
-		Key:  key,
-		Data: data,
-	}
-	return
-}
-
 // Insert : Inserts a new node on the tree, if there's already a Root, it calls InserNode
-func (tree *Tree) Insert(key int, data string) {
+func (tree *Tree) Insert(key int, data string) func() {
 	if tree.Root == nil {
-		tree.InsertRoot(key, data)
-		return
+		tree.Root = &Node{
+			Key:  key,
+			Data: data,
+		}
+		return nil
 	}
-	tree.Root.InsertNode(key, data)
+	return tree.Root.InsertNode(key, data)
 }
 
 // InsertNode : Inserts a new children on the node
 func (node *Node) InsertNode(key int, data string) func() {
+	if node == nil {
+		return nil
+	}
+
 	switch {
 	case key == node.Key:
 		return nil
@@ -60,34 +60,78 @@ func (node *Node) InsertNode(key int, data string) func() {
 	return nil
 }
 
-func printNode(node *Node) {
-	fmt.Print("Key: ", node.Key)
-	if node.Left != nil {
-		fmt.Print(" Left: ", node.Left.Key)
-	} else {
-		fmt.Print(" Left is empty ")
+// Find : Searches for a key and returns its Data
+func Find(node *Node, key int) func() {
+	if node.Key == key {
+		fmt.Println(node.Data)
+		return nil
 	}
-	if node.Right != nil {
-		fmt.Print(" Right: ", node.Right.Key)
-	} else {
-		fmt.Print(" Right is empty ")
+	if node.Key < key {
+		return Find(node.Right, key)
 	}
-	fmt.Println()
+	return Find(node.Left, key)
 }
 
-func display(tree *Tree) {
-	if tree.Root != nil {
-		fmt.Println("Root Key: ", tree.Root.Key)
-		fmt.Println("Title Tree: ", tree.Root.Data)
+func Remove(node *Node, key int) *Node {
+	if node == nil {
+		return nil
 	}
+	if node.Key > key {
+		node.Left = Remove(node.Left, key)
+		return node
+	}
+	if node.Key < key {
+		node.Right = Remove(node.Right, key)
+		return node
+	}
+	if node.Left == nil && node.Right == nil {
+		node = nil
+		return nil
+	}
+	if node.Left == nil {
+		node = node.Right
+		return node
+	}
+	if node.Right == nil {
+		node = node.Left
+		return node
+	}
+	findSmall := node.Right
+	for {
+		if findSmall != nil && findSmall.Left != nil {
+			findSmall = findSmall.Left
+		} else {
+			break
+		}
+	}
+	node.Key, node.Data = findSmall.Key, findSmall.Data
+	node.Right = Remove(node.Right, node.Key)
+	return node
+}
+
+// COUNT : for display the tree
+var COUNT = 10
+
+func display(node *Node, space int) {
+	if node != nil {
+		space += COUNT
+		display(node.Right, space)
+		fmt.Print("\n")
+		for i := COUNT; i < space; i++ {
+			fmt.Print(" ")
+		}
+		fmt.Printf("%d\n", node.Key)
+		display(node.Left, space)
+	}
+	return
 }
 
 func main() {
 	tree := new(Tree)
-	arr := [6]int{1, 2, 3, 4, 5, 6}
-	arrD := [6]string{"Pedro", "Juan", "Diego", "Nutella", "Rain", "Names"}
-	for i := 0; i < len(arr); i++ {
-		tree.InsertRoot(arr[i], arrD[i])
+	keys := [7]int{10, 9, 122, 7, 345, 5, 4}
+	datas := [7]string{"a", "b", "c", "d"}
+	for i := 0; i < len(keys); i++ {
+		tree.Insert(keys[i], datas[i])
 	}
-	display(tree)
+	display(tree.Root, 0)
 }
